d8d0f6a0cbc13c1262829ec2dccd376bb8eaa33f
Kostas Milonas <milonas.ko@gmail.com>
2019-06-16 11:00:33 +0300

    template service removes non-matching placeholders

--- src/Service/TemplateService.php
+++ src/Service/TemplateService.php
@@ -8,6 +8,11 @@ class TemplateService
     {
         $templateContent = file_get_contents(sprintf("%s/../../templates/%s.tpl", __DIR__, $template));
 
-        return str_replace(array_keys($data), array_values($data), $templateContent);
+        $output = str_replace(array_keys($data), array_values($data), $templateContent);
+
+        // remove placeholders that had no match with array keys of $data
+        $output = preg_replace("/%(\w+)/", "", $output);
+
+        return $output;
     }
 }
